a9edd703bb755c07c3b3556a4836e60244cec1a2
rbn42 <bl100@students.waikato.ac.nz>
2017-12-02 18:38:26 +1300

    timezone

--- Stopwatch/sideturn.lua
+++
@@ -1,9 +0,0 @@
-local result =
-    wesnoth.synchronize_choice(
-    function()
-        local t0 = wesnoth.get_time_stamp()
-        return {value = t0}
-    end
-)
-local vname = "Stopwatch_start" .. wesnoth.current.side
-wesnoth.set_variable(vname, result.value)
